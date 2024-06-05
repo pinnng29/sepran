bf6edd48e17415c459e01e70a1fc65e486dffa3f
Aping <apingspeedholic@gmail.com>
2024-06-05 12:48:13 +0700

    Account GET API

---
+++ app/api/[[...route]]/accounts.ts
@@ -0,0 +1,26 @@
+import { Hono } from "hono";
+
+import { db } from "@/db/drizzle";
+import { accounts } from "@/db/schema";
+import { clerkMiddleware, getAuth } from "@hono/clerk-auth";
+import { eq } from "drizzle-orm";
+
+const app = new Hono().get("/", clerkMiddleware(), async (ctx) => {
+  const auth = getAuth(ctx);
+
+  if (!auth?.userId) {
+    return ctx.json({ error: "Unauthorized" }, 401);
+  }
+
+  const data = await db
+    .select({
+      id: accounts.id,
+      name: accounts.name,
+    })
+    .from(accounts)
+    .where(eq(accounts.userId, auth.userId));
+
+  return ctx.json({ data });
+});
+
+export default app;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,10 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+
 import "./globals.css";
-import { ThemeProvider } from "@/providers/theme-provider";
+
 import { ClerkProvider } from "@clerk/nextjs";
+import { QueryProvider } from "@/providers/query-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,14 +22,9 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en">
         <body className={inter.className}>
-          <ThemeProvider
-            attribute="class"
-            defaultTheme="system"
-            enableSystem
-            disableTransitionOnChange
-          >
+          <QueryProvider>
             {children}
-          </ThemeProvider>
+          </QueryProvider>
         </body>
       </html>
     </ClerkProvider>
