1730908bc936371e0d500bc086c2885a6719bdee
Aping <apingspeedholic@gmail.com>
2024-06-07 04:09:22 +0700

    Transaction API

--- db/schema.ts
+++ db/schema.ts
@@ -1,6 +1,7 @@
+import { z } from "zod";
+import { relations } from "drizzle-orm";
 import { createInsertSchema } from "drizzle-zod";
-import { pgTable, text } from "drizzle-orm/pg-core";
-
+import { integer, pgTable, text, timestamp } from "drizzle-orm/pg-core";
 
 export const accounts = pgTable("accounts", {
   id: text("id").primaryKey(),
@@ -8,13 +9,51 @@ export const accounts = pgTable("accounts", {
   userId: text("user_id").notNull(),
 });
 
-export const insertAccountSchema = createInsertSchema(accounts);
+export const accountsRelations = relations(accounts, ({ many }) => ({
+  transactions: many(transactions),
+}));
 
+export const insertAccountSchema = createInsertSchema(accounts);
 
 export const categories = pgTable("categories", {
   id: text("id").primaryKey(),
   name: text("name").notNull(),
   userId: text("user_id").notNull(),
-})
+});
+
+export const categoriesRelations = relations(categories, ({ many }) => ({
+  transactions: many(transactions),
+}));
 
 export const insertCategorySchema = createInsertSchema(categories);
+
+export const transactions = pgTable("transactions", {
+  id: text("id").primaryKey(),
+  amount: integer("amount").notNull(),
+  payee: text("payee").notNull(),
+  notes: text("notes"),
+  date: timestamp("date", { mode: "date" }).notNull(),
+  accountId: text("account_id")
+    .references(() => accounts.id, {
+      onDelete: "cascade",
+    })
+    .notNull(),
+  categoryId: text("category_id").references(() => categories.id, {
+    onDelete: "set null",
+  }),
+});
+
+export const transactionsRelations = relations(transactions, ({ one }) => ({
+  accounts: one(accounts, {
+    fields: [transactions.accountId],
+    references: [accounts.id],
+  }),
+  categories: one(categories, {
+    fields: [transactions.categoryId],
+    references: [categories.id],
+  }),
+}));
+
+export const insertTransactionSchema = createInsertSchema(transactions, {
+  date: z.coerce.date(),
+});
